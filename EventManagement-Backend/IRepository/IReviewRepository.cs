1b915426bd637b735a05ba43738debd955beea4e
Rama2003S <sunkarirama2003@gmail.com>
2024-10-15 11:36:52 +0530

    changing naming

---
+++ EventManagement-Backend/IRepository/IReviewRepository.cs
@@ -0,0 +1,13 @@
+﻿using EventManagement_Backend.Models;
+
+namespace EventManagement_Backend.IRepository
+{
+    public interface IReviewRepository
+    {
+        string AddReview(Review review);
+        string DeleteReview(int reviewId);
+        string UpdateReview(Review review);
+        List<Review> GetAllReviews();
+        Review GetReviewsByEventId(int eventId);
+    }
+}
